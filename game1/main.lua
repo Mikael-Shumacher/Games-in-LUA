495b06a9adde525c3a7ac7735a61e9c527f6d0d3
Mikael Shumacher <mikaelabrell50@gmail.com>
2025-09-14 23:31:10 -0300

    initial commit

---
+++ game1/main.lua
@@ -0,0 +1,31 @@
+function love.load()
+	player = {}
+	velocity = 2
+	player.x = 400 
+	player.y = 200
+	x, y, w,h = 0, 0, 60, 20
+end
+
+function love.update(dt)
+	w = w+20
+	h = h+20
+	if love.keyboard.isDown("s") then
+		player.y = player.y + velocity
+	end
+	if love.keyboard.isDown("w") then
+		player.y = player.y - velocity
+	end
+	if love.keyboard.isDown("d") then
+		player.x = player.x + velocity
+	end
+	if love.keyboard.isDown("a") then
+		player.x = player.x - velocity
+	end
+end
+
+function love.draw()
+	love.graphics.setColor(0, 100, 100)
+	love.graphics.rectangle("fill", x, y, w, h)
+	love.graphics.setColor(100, 0, 0)
+	love.graphics.circle("fill", player.x, player.y, 100)
+end
